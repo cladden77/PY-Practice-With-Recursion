4be662325101db783d204bf7998ed2f12224388f
Chris L <chris.ladden@gmail.com>
2022-09-18 11:48:20 -0500

    created functions for solutions

--- solution1.py
+++ solution1.py
@@ -1,1 +1,14 @@
-# Write code for algorithm 1 below
+# Write code for algorithm 1 below: Write a program that takes a positive number as an argument and prints the numbers from n to zero.
+def count_down(n):
+
+  # Base case
+  if n==0:
+      return
+
+  # Recursive case
+  else:
+      print(n)
+      count_down(n-1)
+
+n=8
+count_down(n)
